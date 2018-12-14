7eeb324e98e390a33605932583cce6c1d50c0924
mechanicheart <34300844+mechanicheart@users.noreply.github.com>
2018-12-14 21:17:30 +0800

    Add files via upload

---
+++ NSFReader.cpp
@@ -0,0 +1,75 @@
+#include "NSFReader.h"
+
+#define read_nsf(num,fp) fread(p,1,num,fp)
+#define extra_link(s) strcat(nsf[pos].extra,s)
+
+void string_filter(char s[],char d[])
+{
+	int i , L = strlen(s);
+	for(i=0;i<L;i++)
+	{
+		if(s[i]>=FILTER_INF&&s[i]<=FILTER_SUP)
+			d[i] = s[i];
+		else break;
+	}
+	return;
+}
+
+int main()
+{
+	long file;
+	_chdir(ADDRESS);
+	int i , pos = 0;
+	if((file = _findfirst("*.*", &find))==-1L)
+	{
+		printf("空白!\n");
+		return -1;
+	}
+	while(_findnext(file, &find) == 0)
+	{
+		FILE *fp= fopen(find.name,"r");
+		strcpy(nsf[pos].chs_name,find.name);
+		if(!M_Load(fp,HEADER_TITLE))						//将文件指针移至Title处
+		{
+			//p =(char*)calloc(Max_Size,sizeof(char));
+			read_nsf(NSF_NAME_LIMIT,fp);
+			//strcpy(nsf[pos].org_name,p);
+			string_filter(p,nsf[pos].org_name);
+		}
+		if(!M_Load(fp,HEADER_ARTIST))					//将文件指针移至Artist处
+		{
+			read_nsf(NSF_NAME_LIMIT,fp);
+			string_filter(p,nsf[pos].artist);
+		}
+		if(!M_Load(fp,HEADER_COPYRIGHT))			//将文件指针移至Copyright处
+		{
+			read_nsf(NSF_NAME_LIMIT,fp);
+			string_filter(p,nsf[pos].copyright);
+		}
+		if(!M_Load(fp,HEADER_CHIP))
+		{
+			read_nsf(1,fp);
+			strcpy(nsf[pos].extra,"2A03");
+			if((p[0] & APU_VRC6)!=0)
+				extra_link("+VRC6");
+			if((p[0] & APU_VRC7)!=0)
+				extra_link("+VRC7");
+			if((p[0] & APU_FDS)!=0)
+				extra_link("+FDS");
+			if((p[0] & APU_MMC5)!=0)
+				extra_link("+MMC5");
+			if((p[0] & APU_N163)!=0)
+				extra_link("+N163");
+			if((p[0] & APU_5B)!=0)
+				extra_link("+SUN5B");
+		}
+		pos++;
+		fclose(fp);
+	}
+	_findclose(file);
+	FILE *wp = fopen(Save_File,"w");
+	for(i = 0;i < pos;i++)
+		fprintf(wp,Output_Format,nsf[i].chs_name,nsf[i].org_name,nsf[i].artist,nsf[i].copyright,nsf[i].extra);
+	//printf("%d",L);
+	return 0;
+}
---
+++ NSFReader.h
@@ -0,0 +1,46 @@
+#ifndef _NSFREADER
+#define _NSFREADER
+
+#include<io.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+#define Max_Size 2000
+#define NAME_LIMIT 100
+#define NSF_NAME_LIMIT 32
+#define ADDRESS "d:\\Famitracker制作\\FC BGM NSF\\123"
+#define Save_File "NSFReader.txt"
+#define Output_Format "%s\t\t%s\t\t%s\t\t%s\t\t%s\n"
+
+#define APU_VRC6 0x01
+#define APU_VRC7 0x02
+#define APU_FDS 0x04
+#define APU_MMC5 0x08
+#define APU_N163 0x10
+#define APU_5B 0x20
+
+#define HEADER_TITLE 0x0E
+#define HEADER_ARTIST 0x2E
+#define HEADER_COPYRIGHT 0x4E
+#define HEADER_CHIP 0x7B
+
+#define FILTER_SUP 0x7A
+#define FILTER_INF 0x20
+
+#define M_Load(fp,address) fseek(fp,address,0)
+struct nsf_file
+{
+	char chs_name[NAME_LIMIT];
+	char org_name[NSF_NAME_LIMIT];
+	char artist[NSF_NAME_LIMIT];
+	char copyright[NSF_NAME_LIMIT];
+	char extra[NSF_NAME_LIMIT];
+}nsf[Max_Size];														//实际上不一定需要保存，可以实时输出
+
+struct _finddata_t find;
+
+char p[Max_Size];
+extern void string_filter(char s[],char d[]);
+
+#endif
